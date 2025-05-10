be653a8d1612c770662dd6588a064bd1a6c3e40a
Eduardo <eduao154@gmail.com>
2025-05-10 00:24:07 -0400

    New Commit-2
    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AppProvider from "./Context/Context.jsx";
 import { ThemeProvider } from "./Context/Themes.jsx";
 import MainPage from "./Pages/MainPage.jsx";
@@ -8,11 +8,13 @@ function App() {
   return (
     <ThemeProvider>
       <AppProvider>
-        <BrowserRouter basename="/Managment-UI">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-          </Routes>
-        </BrowserRouter>
+ 
+<Router basename="/Managment-UI">
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+  </Routes>
+</Router>
+
       </AppProvider>
     </ThemeProvider>
   );
